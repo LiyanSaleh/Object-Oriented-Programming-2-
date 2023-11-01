ffd2a2f52707815d4439780e6bd728779c2b4508
LiyanSaleh <148647521+LiyanSaleh@users.noreply.github.com>
2023-11-01 19:35:06 +0300

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,11 @@
+public class Multi{
+    public static void main(String args[]){
+        int size=5;
+    for(int i=1; i<=size ; i++){
+	for(int j=1 ; j<=10 ; j++){
+	System.out.print(i * j + " ");
+	}
+        System.out.println();
+    }
+    }
+}
