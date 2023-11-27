645f635d6c112c7753464e2b54505de33da9338a
LiyanSaleh <148647521+LiyanSaleh@users.noreply.github.com>
2023-11-27 19:30:14 +0300

    Delete oop2_Week7/src/main/java/oop2_week7 directory

--- oop2_Week7/src/main/java/oop2_week7/FacadeExample.java
+++
@@ -1,7 +0,0 @@
-package oop2_week7;
-public class FacadeExample {
-    public static void main (String args[]){
-        Facade facade= new Facade();
-        facade.operate();
-    }
-}
--- oop2_Week7/src/main/java/oop2_week7/Subsystem1.java
+++
@@ -1,6 +0,0 @@
-package oop2_week7;
-public class Subsystem1 {
-    public void operation1(){
-        System.out.println("SubSystem1:Operation 1");
-    }
-}
